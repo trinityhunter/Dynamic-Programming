0adae44a3d944ebc388d76c71f09fde7eb2522de
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2023-06-14 21:07:32 +0530

    Create 4 - 516. Longest Palindromic Subsequence.java

---
+++ DP on Strings/4 - 516. Longest Palindromic Subsequence.java
@@ -0,0 +1,99 @@
+class Solution {
+
+    // public static int helper(String x, String y, int ind1, int ind2){
+
+    //     if(ind1 == 0 || ind2 == 0){
+    //         return 0;
+    //     }
+
+    //     if(dp[ind1][ind2]!=null){
+    //         return dp[ind1][ind2];
+    //     }
+
+    //     if(x.charAt(ind1-1) == y.charAt(ind2-1)){
+    //         return dp[ind1][ind2] = 1 + helper(x, y, ind1-1, ind2-1);
+    //     }
+
+    //     return dp[ind1][ind2] = Math.max(helper(x, y, ind1-1, ind2), helper(x, y, ind1, ind2-1));
+
+    // }
+
+    // public static Integer[][] dp;
+
+    public int longestPalindromeSubseq(String s) {
+
+        // ***** Memoization ***** - 
+        
+        // StringBuilder sb = new StringBuilder(s);
+
+        // sb = sb.reverse();
+
+        // dp = new Integer[s.length()+1][sb.toString().length()+1];
+
+        // int ans = helper(s, sb.toString(), s.length(), sb.toString().length());
+
+        // return ans;
+
+        // ***** Tabulation ***** - 
+
+        // StringBuilder sb = new StringBuilder(s);
+
+        // sb = sb.reverse();
+
+        // int[][] dp = new int[s.length()+1][sb.toString().length()+1];
+
+        // for(int i=0; i<dp.length; i++){
+        //     for(int j=0; j<dp[0].length; j++){
+        //         if(i == 0){
+        //             dp[i][j] = 0;
+        //         }
+        //         else if(j == 0){
+        //             dp[i][j] = 0;
+        //         }
+        //         else{
+        //             if(s.charAt(i-1) == sb.toString().charAt(j-1)){
+        //                 dp[i][j] = 1 + dp[i-1][j-1];
+        //             }
+        //             else{
+        //                 dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+        //             }
+        //         }
+        //     }
+        // }
+
+        // return dp[dp.length-1][dp[0].length-1];
+
+        // ***** Space Optimized ***** - 
+
+        StringBuilder sb = new StringBuilder(s);
+
+        sb = sb.reverse();
+
+        int[] prev = new int[sb.toString().length()+1];
+
+        for(int i=0; i<s.length()+1; i++){
+            int[] curr = new int[sb.toString().length()+1];
+            for(int j=0; j<s.length()+1; j++){
+                if(i == 0){
+                    curr[j] = 0;
+                }
+                else if(j == 0){
+                    curr[j] = 0;
+                }
+                else{
+                    if(s.charAt(i-1) == sb.toString().charAt(j-1)){
+                        curr[j] = 1 + prev[j-1];
+                    }
+                    else{
+                        curr[j] = Math.max(prev[j], curr[j-1]);
+                    }
+                }
+            }
+            prev = curr;
+        }
+
+        return prev[s.length()];
+
+    }
+    
+}
