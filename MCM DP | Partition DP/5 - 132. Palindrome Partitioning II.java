28cc4a9fc6ef6d4f3d279c8d7d76b57bbaf26326
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2023-06-17 22:07:14 +0530

    Create 5 - 132. Palindrome Partitioning II.java

---
+++ MCM DP | Partition DP/5 - 132. Palindrome Partitioning II.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public int minCut(String s) {
+
+        // ***** Tabulation ***** - 
+
+        int n = s.length();
+        
+        int[] cut = new int[s.length()+1];
+        
+        for(int i=0; i<=n; i++){
+            cut[i] = i-1;
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; i-j>=0 && i+j<n && s.charAt(i-j)==s.charAt(i+j); j++){
+                cut[i+j+1] = Math.min(cut[i+j+1],1+cut[i-j]);
+            }
+            for(int j=1; i-j+1>=0 && i+j<n && s.charAt(i-j+1) == s.charAt(i+j); j++){
+                cut[i+j+1] = Math.min(cut[i+j+1],1+cut[i-j+1]);
+            }
+        }
+
+        return cut[n];
+
+    }
+    
+}
